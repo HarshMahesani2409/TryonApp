cbbebe88c3768ca662f89c8f386d6a6e2f0d00de
HarshMahesani2409 <harshmahesani24@gmail.com>
2026-04-22 12:41:51 +0530

    Add files via upload

---
+++ Zmain.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, Response,redirect
+from camera import VideoCamera
+import os
+app = Flask(__name__)
+
+CART=[]
+
+@app.route('/tryon/<file_path>',methods = ['POST', 'GET'])
+def tryon(file_path):
+	file_path = file_path.replace(',','/')
+	os.system('python tryOn.py ' + file_path)
+	return redirect('http://127.0.0.1:5000/',code=302, Response=None)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run()
+    
---
+++ tryOn.py
@@ -0,0 +1,244 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+import cv2, threading, os, time
+from threading import Thread
+from os import listdir
+from os.path import isfile, join
+
+import dlib
+from imutils import face_utils, rotate_bound
+import math
+
+
+def put_sprite(num):
+    global SPRITES, BTNS
+    
+    SPRITES[num-1] = (1 - SPRITES[num]) 
+    # if SPRITES[num]:
+    #     BTNS[num].config(relief=SUNKEN)
+    # else:
+    #     BTNS[num].config(relief=RAISED)
+
+def draw_sprite(frame, sprite, x_offset, y_offset):
+    (h,w) = (sprite.shape[0], sprite.shape[1])
+    (imgH,imgW) = (frame.shape[0], frame.shape[1])
+
+    if y_offset+h >= imgH:
+        sprite = sprite[0:imgH-y_offset,:,:]
+
+    if x_offset+w >= imgW:
+        sprite = sprite[:,0:imgW-x_offset,:]
+
+    if x_offset < 0: 
+        sprite = sprite[:,abs(x_offset)::,:]
+        w = sprite.shape[1]
+        x_offset = 0
+
+    for c in range(3):
+            frame[y_offset:y_offset+h, x_offset:x_offset+w, c] =  \
+            sprite[:,:,c] * (sprite[:,:,3]/255.0) +  frame[y_offset:y_offset+h, x_offset:x_offset+w, c] * (1.0 - sprite[:,:,3]/255.0)
+    return frame
+
+def adjust_sprite2head(sprite, head_width, head_ypos, ontop = True):
+    (h_sprite,w_sprite) = (sprite.shape[0], sprite.shape[1])
+    factor = 1.0*head_width/w_sprite
+    sprite = cv2.resize(sprite, (0,0), fx=factor, fy=factor)
+    (h_sprite,w_sprite) = (sprite.shape[0], sprite.shape[1])
+
+    y_orig =  head_ypos-h_sprite if ontop else head_ypos 
+    if (y_orig < 0):
+            sprite = sprite[abs(y_orig)::,:,:] 
+            y_orig = 0
+    return (sprite, y_orig)
+def apply_sprite2feature(image, sprite_path, haar_filter, x_offset, y_offset, y_offset_image, adjust2feature, desired_width, x, y, w, h):
+    sprite = cv2.imread(sprite_path,-1)
+    (h_sprite,w_sprite) = (sprite.shape[0], sprite.shape[1])
+
+    xpos = x + x_offset
+    ypos = y + y_offset
+    factor = 1.0*desired_width/w_sprite
+
+    sub_img = image[y + y_offset_image:y+h,x:x+w,:]
+
+    feature = apply_Haar_filter(sub_img, haar_filter, 1.3 , 10, 10)
+    if len(feature)!=0:
+        xpos, ypos = x, y + feature[0,1] #adjust only to feature in y axis (eyes)
+
+        if adjust2feature:
+            size_mustache = 1.2 #how many times bigger than mouth
+            factor = 1.0*(feature[0,2]*size_mustache)/w_sprite
+            xpos =  x + feature[0,0] - int(feature[0,2]*(size_mustache-1)//2) #centered respect to width
+            ypos = y + y_offset_image + feature[0,1] - int(h_sprite*factor) #right on top
+
+    sprite = cv2.resize(sprite, (0,0), fx=factor, fy=factor)
+    image = draw_sprite(image,sprite,xpos,ypos)
+
+def apply_sprite(image, path2sprite,w,x,y, angle, ontop = True):
+    sprite = cv2.imread(path2sprite,-1)
+    sprite = rotate_bound(sprite, angle)
+    (sprite, y_final) = adjust_sprite2head(sprite, w, y, ontop)
+    image = draw_sprite(image,sprite,x, y_final)
+
+def calculate_inclination(point1, point2):
+    x1,x2,y1,y2 = point1[0], point2[0], point1[1], point2[1]
+    incl = 180/math.pi*math.atan((float(y2-y1))/(x2-x1))
+    return incl
+def apply_Haar_filter(img, haar_cascade,scaleFact = 1.05, minNeigh = 3, minSizeW = 30):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    features = haar_cascade.detectMultiScale(
+        gray,
+        scaleFactor=scaleFact,
+        minNeighbors=minNeigh,
+        minSize=(minSizeW, minSizeW),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    return feature
+
+def calculate_boundbox(list_coordinates):
+    x = min(list_coordinates[:,0])
+    y = min(list_coordinates[:,1])
+    w = max(list_coordinates[:,0]) - x
+    h = max(list_coordinates[:,1]) - y
+    return (x,y,w,h)
+
+def detectUpperBody(image):
+    cascadePath = "data/haarcascade_upperbody.xml"
+    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cascade = cv2.CascadeClassifier(cascadePath)
+    Rect = cascade.detectMultiScale(imageGray, scaleFactor=1.1, minNeighbors=1, minSize=(1,1)) 
+    if len(Rect) <= 0:
+	    return False	
+    else:
+	    return Rect
+
+def get_face_boundbox(points, face_part):
+    if face_part == 1:
+        (x,y,w,h) = calculate_boundbox(points[17:22])
+    elif face_part == 2:
+        (x,y,w,h) = calculate_boundbox(points[22:27]) 
+    elif face_part == 3:
+        (x,y,w,h) = calculate_boundbox(points[36:42])
+    elif face_part == 4:
+        (x,y,w,h) = calculate_boundbox(points[42:48])
+    elif face_part == 5:
+        (x,y,w,h) = calculate_boundbox(points[29:36])
+    elif face_part == 6:  
+        (x,y,w,h) = calculate_boundbox(points[1:17])
+    elif face_part == 7:
+         (x,y,w,h) = calculate_boundbox(points[0:6])
+    elif face_part == 8:
+        (x,y,w,h) = calculate_boundbox(points[11:17])
+    return (x,y,w,h)
+
+
+image_path = ''
+
+def add_sprite(img):
+    global image_path,SPRITES
+    image_path = img
+    # print(img.rsplit('/',1))
+    put_sprite(int(img.rsplit('/',1)[0][-1]))
+    print(SPRITES)
+#Principal Loop where openCV (magic) ocurs
+def cvloop(run_event):
+    global panelA
+    global SPRITES
+    global image_path
+    video_capture = cv2.VideoCapture(0) #read from webcam
+    video_capture.set(3,2048)
+    video_capture.set(4,2048)
+    (x,y,w,h) = (0,0,10,10) #whatever initial values
+
+    #Filters path
+    detector = dlib.get_frontal_face_detector()
+    fullbody = cv2.CascadeClassifier('data/haarcascade_fullbody.xml')
+    model = "data/shape_predictor_68_face_landmarks.dat"
+    predictor = dlib.shape_predictor(model) # link to model: http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+    print(SPRITES)
+    while run_event.is_set(): 
+        ret, image = video_capture.read()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray, 0)
+        
+        for face in faces: 
+            (x,y,w,h) = (face.left(), face.top(), face.width(), face.height())
+            
+            shape = predictor(gray, face)
+            shape = face_utils.shape_to_np(shape)
+            incl = calculate_inclination(shape[17], shape[26]) #inclination based on eyebrows
+
+            
+            if SPRITES[0]:#neck
+                (x1,y1,w1,h1) = get_face_boundbox(shape, 6)
+                apply_sprite(image, image_path,w1,x1,y1+150, incl,  ontop = False)
+                print(1)
+        
+            (x0,y0,w0,h0) = get_face_boundbox(shape, 6)
+            if SPRITES[1]:#Earing
+                (x3,y3,w3,h3) = get_face_boundbox(shape, 7) #nose
+                apply_sprite(image, image_path,w3,x3-40,y3+30, incl,ontop=False)
+                (x3,y3,w3,h3) = get_face_boundbox(shape, 8) #nose
+                apply_sprite(image, image_path,w3,x3+40,y3+75, incl)              
+                print(2)
+                
+            if SPRITES[2]:#Tiaras
+                apply_sprite(image, image_path,w+45,x-20,y+20, incl, ontop = True)
+                print(3)
+
+            if SPRITES[3]:#Tops
+                #(x,y,w,h) = (0,0,10,10)
+                #apply_sprite2feature(image, image_path, fullbody, w//4, 2*h//3, h//2, True, w//2, x, y, w, h)
+                (x1,y1,w1,h1) = get_face_boundbox(shape, 8)
+                apply_sprite(image, image_path,w1+350,x1-230,y1+100, incl,  ontop = False)
+                print(4)
+            
+
+            if SPRITES[4]:#frocks
+                (x1,y1,w1,h1) = get_face_boundbox(shape, 8)
+                apply_sprite(image, image_path,w1+580,x1-350,y1+80, incl,  ontop = False)
+                print(5)
+                
+            if SPRITES[5]:#gogles
+                (x3,y3,_,h3) = get_face_boundbox(shape, 1)
+                apply_sprite(image, image_path,w,x,y3-10, incl, ontop = False)
+                print(6)
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        image = ImageTk.PhotoImage(image)
+        panelA.configure(image=image)
+        panelA.image = image
+
+    video_capture.release()
+
+# Initialize GUI object
+root = Tk()
+root.title("E-Dressing- Face")
+this_dir = os.path.dirname(os.path.realpath(__file__))
+btn1 = None
+
+def try_on(image_path):
+    btn1 = Button(root, text="Try it ON", command = lambda:add_sprite(image_path))
+    btn1.pack(side="top", fill="both", expand="no", padx="5", pady="5")
+panelA = Label(root)
+panelA.pack( padx=10, pady=10)
+SPRITES = [0,0,0,0,0,0,0]
+
+BTNS = [btn1]
+try_on(sys.argv[1])
+run_event = threading.Event()
+run_event.set()
+action = Thread(target=cvloop, args=(run_event,))
+action.setDaemon(True)
+action.start()
+
+def terminate():
+        global root, run_event, action
+        run_event.clear()
+        time.sleep(1)
+        root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", terminate)
+root.mainloop() 
